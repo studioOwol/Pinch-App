956ca740413763aa78b252d3ab05a8b08cb7ed71
studioOwol <from.owol@gmail.com>
2023-04-17 20:15:49 +0900

    Pinch-App Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Nahyun on 2023/04/17.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
