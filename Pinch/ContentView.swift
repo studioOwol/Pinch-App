718955e6669007114193ecef34336a6e774fb0e4
studioOwol <from.owol@gmail.com>
2023-04-18 14:01:43 +0900

    Settings

--- Pinch/ContentView.swift
+++ Pinch/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            // Test
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
