979eaf4d8b8654543982b7eec11c0e71a99250b6
studioOwol <from.owol@gmail.com>
2023-04-18 15:18:35 +0900

    Control Interface: 확대 축소 원본크기 인터페이스

--- Pinch/ContentView.swift
+++ Pinch/ContentView.swift
@@ -79,6 +79,53 @@ struct ContentView: View {
                     .padding(.top, 30)
                    , alignment: .top
             )
+            //MARK: - CONTROLS
+            .overlay(
+                Group{
+                    HStack{
+                    // Scale Down
+                        Button{
+                            withAnimation(.spring()){
+                                if imageScale > 1{
+                                    imageScale -= 1
+                                    if imageScale <= 1{
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                    // Reset
+                        Button{
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                    
+                    // Scale UP
+                        Button{
+                            withAnimation(.spring()){
+                                if imageScale < 5 {
+                                    imageScale += 1
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                        
+                    } //: Controls
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1:0)
+                }
+                    .padding(.bottom, 30)
+                ,   alignment: .bottom
+            )
         }//: Navigation
         .navigationViewStyle(.stack)
     }
